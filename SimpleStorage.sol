daaf38cecaa64c5f1d0d5d44eeb3e2df82f40c94
yimu-ctrl <2806685503qq@gmail.com>
2025-04-25 21:48:46 +0800

    update

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19; // this is the solidity version
 
 contract SimpleStorage {
     // favoriteNumber gets initialized to 0 if no value is given
-    uint256 public favoriteNumber; // 0
+    uint256 favoriteNumber; // 0
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
